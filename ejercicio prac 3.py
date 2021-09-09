2f118b95e859975cadc3766bdb7cb4139f09057c
fiorescalante <fior_escalante@hotmail.com>
2021-09-09 19:33:32 -0300

    primer commit

---
+++ ejercicio prac 3.py
@@ -0,0 +1,25 @@
+n= int(input("ingrese la cant de terminos: "))
+cont=0
+i=1
+sum=0
+while cont<n:
+
+    if i % 2!=0:
+        cont+=1
+        print(cont, sum)
+        if cont%2==0:
+
+            sum-=1/i
+            print("resta", sum)
+
+
+
+        else:
+            sum+=1/i
+            print("suma", sum)
+
+
+    i+=1
+print(sum)
+
+#1/0! + 1/2! + 1/3!
